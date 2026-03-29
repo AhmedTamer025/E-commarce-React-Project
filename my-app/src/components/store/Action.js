2c1db6c373ed9ea05cb65d4cc47128da41a67fae
Ahmed Tamer <125506462+AhmedTamer025@users.noreply.github.com>
2026-03-29 21:40:15 +0200

    Create Action.js

---
+++ my-app/src/components/store/Action.js
@@ -0,0 +1,14 @@
+
+export function quantitynumm(quantitynum) {
+    return {
+      type: 'SET_QUANTITY',
+      payload: quantitynum,
+    };
+  }
+
+export function addToCart(cartData){
+    return{
+        type:'SET_CART',
+        payload:cartData
+    }
+}
